89bd5eb458ae9fc9d91077cf9cbda127213f7d3b
John <j842@users.noreply.github.com>
2018-05-20 16:18:46 +1200

    playing with go

---
+++ go/hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+	fmt.Println("My favorite number is", math.Sqrt(float64(rand.Intn(10))))
+}
